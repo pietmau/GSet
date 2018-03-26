ce9e4aafe6f1772346d90b829706029daa9e5a81
Maurizio Pietrantuono <maurizio.pietrantuon@Mac2210.local>
2018-03-26 11:30:07 +0100

    Draw Diamond

--- Set/View/CardView.swift
+++ Set/View/CardView.swift
@@ -36,21 +36,76 @@ class CardView: UIView {
 
     private func drawSymbols() {
         let rects = getRecs()
-        
         for rect in rects {
-            let cardbackGround = UIBezierPath(roundedRect: rect, cornerRadius: CGFloat(10))
-            UIColor.blue.setFill()
-            cardbackGround.fill()
+            drawShapeInRec(rect)
         }
     }
 
+    private func drawShapeInRec(_ rect: CGRect) {
+        let shape = getShape(rect)
+        colorShape()
+        fillShape(shape)
+    }
+
+    private func getShape(_ rect: CGRect) -> UIBezierPath {
+        if (card?.shape == Shape.FIRST) {
+            return getDiamond(rect)
+        }
+        if (card?.shape == Shape.SECOND) {
+            return getDiamond(rect)
+        }
+        if (card?.shape == Shape.THIRD) {
+            return getDiamond(rect)
+        }
+        fatalError("Invalid shape")
+    }
+
+    private func getDiamond(_ rect: CGRect) -> UIBezierPath {
+        let path = UIBezierPath()
+        let halfY = rect.minY + rect.height / 2
+        let halfX = rect.minX + rect.width / 2
+        let startPoint = CGPoint(x: rect.minX, y: halfY)
+        path.move(to: startPoint)
+        path.addLine(to: CGPoint(x: halfX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: halfY))
+        path.addLine(to: CGPoint(x: halfX, y: rect.maxY))
+        path.close()
+        return path
+    }
+
+    private func fillShape(_ shape: UIBezierPath) {
+        shape.fill()
+    }
+
+    private func colorShape() {
+        UIColor.red.setFill()
+    }
+
     private func getRecs() -> [CGRect] {
-            let rect = getFrameWithInset(rect: bounds)
+        var rects: [CGRect] = []
+        let rect = getFrameWithInset(rect: bounds)
+        if (card?.number == 3) {
             let firstDivision = rect.divided(atDistance: rect.height / 3, from: CGRectEdge.minYEdge)
             let secondDivision = getHalf(rect: firstDivision.remainder)
-            var rects: [CGRect] = [firstDivision.slice, secondDivision.slice, secondDivision.remainder]
+            rects = [firstDivision.slice, secondDivision.slice, secondDivision.remainder]
             rects = addInsets(rects)
             return rects
+        }
+        if (card?.number == 1) {
+            let firstDivision = rect.divided(atDistance: rect.height / 3, from: CGRectEdge.minYEdge)
+            let secondDivision = getHalf(rect: firstDivision.remainder)
+            rects = [secondDivision.slice]
+            rects = addInsets(rects)
+            return rects
+        }
+        if (card?.number == 2) {
+            let firstDivision = rect.divided(atDistance: rect.height / 3, from: CGRectEdge.minYEdge)
+            let secondDivision = getHalf(rect: firstDivision.remainder)
+            rects = [firstDivision.slice, secondDivision.remainder]
+            rects = addInsets(rects)
+            return rects
+        }
+        fatalError("Invalid number!")
     }
 
     private func addInsets(_ rects: [CGRect]) -> [CGRect] {
